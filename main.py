b3d947eaa725c44071ab7aaddfe5f6b6acbbcc23
Hernán <carloshernangarrido@gmail.com>
2023-01-10 17:15:14 -0300

    initial commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# python 3.8
+import numpy as np
+import pandas as pd
 
 
 def print_hi(name):
