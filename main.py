0aeea30ed07660f66e920c4a58ab2d4d7fe1637f
Hernán <carloshernangarrido@gmail.com>
2023-01-10 17:17:26 -0300

    initial commit

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ import numpy as np
 import pandas as pd
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
+def main():
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
